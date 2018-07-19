8abf11a44b810b8ac7be521c6ded0a8c133a1887
Piyush Gupta <piyushgupta@Piyushs-MacBook-Pro.local>
2018-07-19 14:24:47 +0530

    section 5 finished

---
+++ Basics Playground.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// var means this value is actually variable and might have it's value changed
+var myAge: Int = 32
+myAge = 33
+
+
+// let means a const
+let name: String = "Piyush" // uncommenting below one will give error
+// name = "Xy"
+
+let myAgeInTenYears: Int = myAge + 10
+let fullName: String = name + " Gupta"
+let piyushAge = "\(name), \(myAge)"
+
+
+let booleans: Bool = true
+let floatingPointNumber: Float = 1.3
+
+let double: Double = 3.14159265423
--- Section 5/Dicee/Dicee/ViewController.swift
+++ Section 5/Dicee/Dicee/ViewController.swift
@@ -13,11 +13,13 @@ class ViewController: UIViewController {
     @IBOutlet weak var imgViewDice1: UIImageView!
     @IBOutlet weak var imgViewDIce2: UIImageView!
     
+    let diceArray = ["dice1","dice2","dice3","dice4","dice5","dice6"]
     var randomDiceVal1 : Int = 0
     var randomDiceVal2 : Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        updateDices()
     }
 
     override func didReceiveMemoryWarning() {
@@ -27,11 +29,20 @@ class ViewController: UIViewController {
 
 
     @IBAction func btnRoll(_ sender: UIButton) {
-        
+        updateDices()
+    }
+    func updateDices(){
         // This random function returns unsignted int 32 hence type casted
         randomDiceVal1 = Int(arc4random_uniform(6))
         randomDiceVal2 = Int(arc4random_uniform(6))
-        print(randomDiceVal1)
+        imgViewDice1.image = UIImage(named: diceArray[randomDiceVal1])
+        imgViewDIce2.image = UIImage(named: diceArray[randomDiceVal2])
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        // we can get the type of event here
+        // if(motion == .motionShake)
+        updateDices()
     }
 }
 
