f2e3454ad17cc32406e963ede04c458402632b0c
pigupta@microsoft.com <pigupta@microsoft.com>
2018-07-26 12:28:36 +0530

    section 11 completed

---
+++ Section 11/Auto Layout Calculator/Auto Layout Calculator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Auto Layout Calculator
+//
+//  Created by Piyush Gupta on 26/07/18.
+//  Copyright © 2018 Piyush Gupta. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func btnClicked(_ sender: UIButton) {
+    }
+    
+}
+
